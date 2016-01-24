b4e47f6fb56afd3500339af92afae9d228b51f2c
Martin Deligny <rojowstyle@gmail.com>
2016-01-24 13:07:59 +0100

    background + scroll spy

--- js/init.js
+++ js/init.js
@@ -4,5 +4,10 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
 
+
+    $(document).ready(function(){
+      $('.scrollspy').scrollSpy();
+    });
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
